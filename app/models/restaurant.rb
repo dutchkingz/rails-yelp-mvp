92753c01b0eddb62851c890c3842030ab31ffe62
vincent mohanna <vince.mohanna@gmail.com>
2026-02-25 12:59:06 +0100

    models are verified

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  # Relationship: When a restaurant is destroyed its reviews are also destroyed
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: [ "chinese", "italian", "japanese", "french", "belgian" ] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  # validates :rating, inclusion: { in: [ 1, 2, 3, 4, 5 ] }
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
 end
