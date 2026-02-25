1a820de46e2d627bef4d23403421f90dbd084b20
vincent mohanna <vince.mohanna@gmail.com>
2026-02-25 16:09:38 +0100

    most of the structure is working now

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,22 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find([ :id ])
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurants_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
