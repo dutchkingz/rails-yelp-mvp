49d64bedb867ea258f336f2cdd723f63c5d5d412
vincent mohanna <vince.mohanna@gmail.com>
2026-02-25 16:55:24 +0100

    some more functionality working now

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,17 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.save
+    # force navigation to go back to current restaurant
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
